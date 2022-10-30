6116dc3b1f2f41db1a261dec8473b0af1b89be5e
bigterboy <ninatabala@gmail.com>
2022-10-30 18:01:43 +0700

    try to connect database when having some issues

--- index.js
+++ index.js
@@ -12,15 +12,22 @@ const app = express();
 
 const mongoUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 
-mongoose
-  //   .connect("mongodb://root:123123@172.25.0.2:27017/?authSource=admin")
-  .connect(mongoUrl)
-  .then(() => {
-    console.log("Succesfully connected to DB");
-  })
-  .catch((e) => console.log(e));
+const connectWithRetry = () => {
+  mongoose
+    //   .connect("mongodb://root:123123@172.25.0.2:27017/?authSource=admin")
+    .connect(mongoUrl)
+    .then(() => {
+      console.log("Succesfully connected to DB");
+    })
+    .catch((e) => {
+      console.log(e);
+      setTimeout(connectWithRetry, 5000);
+    });
+};
 
-app.get("/", (req, res ) => {
+connectWithRetry();
+
+app.get("/", (req, res) => {
   res.send("<h2>HI THERE</h2>");
 });
 
